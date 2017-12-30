cdf2f2f6ce80063e828ba76827f215b06a994ed8
glaux <mirmikh@gmail.com>
2017-12-30 16:11:01 +0530

    Add ShowCard and Search state

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import preload from "../data.json";
+import ShowCard from "./ShowCard";
 
-const Search = () => (
-  <div className="search">
-    {preload.shows.map(show => <h3>{show.title}</h3>)}
-  </div>
-);
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: "debug"
+    };
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div className="search">
+          {preload.shows.map(show => <ShowCard show={show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
