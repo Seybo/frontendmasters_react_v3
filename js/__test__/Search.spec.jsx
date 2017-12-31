0d576dfbd28febe9bc3faba7db61b3537778282a
glaux <mirmikh@gmail.com>
2017-12-31 16:32:19 +0530

    Switch tests to enzyme

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -1,10 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 import Search from "../Search";
 
 test("Search renders correctly", () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
 });
